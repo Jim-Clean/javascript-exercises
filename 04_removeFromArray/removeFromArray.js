9d512826c6f057e89c84de8de9134d80e200ae73
Jim-Clean <128581808+Jim-Clean@users.noreply.github.com>
2023-07-16 11:12:25 +0100

    Finish JS exercises lesson Fund part 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    const result = array.filter(check);
+
+    function check(item) {
+    return item = !args.includes(item);
+    };
+    return result;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//function removes specified input from original array and returns array without input value
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    
+    let sum = 0;
 
+    if (Math.sign(number1) != 1 || Math.sign(number2) != 1 || typeof number1 != 'number' || typeof number2 != 'number'){
+        sum = "ERROR"
+    } else if (number1 > number2){
+        for (var i = number2; i <= number1; i++) {
+            sum += i;
+        };
+    } else {
+        for (var i = number1; i <= number2; i++) {
+            sum += i;
+        };
+    };
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+//when working with numbers need to start stored value at 0!
